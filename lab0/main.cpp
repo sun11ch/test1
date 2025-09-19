0cf9275cab9d87575d84e9b7130bf6966ffb5bf4
sun11ch <drachuk.alexanderr@yandex.ru>
2025-09-19 18:29:27 +0200

    add

--- lab0/main.cpp
+++ lab0/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    int a, b, sum, umn, deln;
+    cout << "Num a:";
+    cin >> a;
+    cout << "Num b:";
+    cin >> b;
 
-    std::cout << "Hello, world!" << std::endl;
-    std::cout << "111" << std::endl;
+    umn = a*b, deln = a/b, sum = a + b;
+
+    cout << "a+b:" << sum << endl;
+    cout << "a/b:" << deln << endl;
+    cout << "a*b:" << umn<< endl;
+   
+
 
     return 0;
 }
