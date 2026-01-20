7473d0b85cd81cea1859aa5adccabe3fc5cc0d16
sun11ch <drachuk.alexanderr@yandex.ru>
2026-01-20 20:55:46 +0200

    Update main.cpp

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <string>
 class Player {
 private:
 	std::string name;
@@ -8,14 +8,6 @@ private:
 	unsigned hp;
 	bool doing;
 public:
-	Player(std::string name_, float x_, float y_, unsigned hp_, bool doing_) {
-		std::cout << "norm player created" << std::endl;
-		name = name_;
-		x = x_;
-		y = y_;
-		hp = hp_;
-		doing = doing_;
-	}
 	Player() {
 		std::cout << "afk player created" << std::endl;
 		name = "Undefined";
@@ -24,7 +16,7 @@ public:
 		hp = 100;
 		doing = 1;
 	}
-	Player(Player &p) {
+	Player(const Player& p) {
 		std::cout << "player copied" << std::endl;
 		name = p.name;
 		x = p.x;
@@ -32,31 +24,39 @@ public:
 		hp = p.hp;
 		doing = p.doing;
 	}
+	Player(std::string name_, float x_, float y_, unsigned hp_, bool doing_) {
+		std::cout << "norm player created" << std::endl;
+		name = name_;
+		x = x_;
+		y = y_;
+		hp = hp_;
+		doing = doing_;
+	}
 	~Player(){
 		std::cout << "player cleared" << std::endl;
 	}
 
-	void printinfo() {
+	void printinfo() const {
 		std::cout << "Name: " << name << std::endl;
 		std::cout << "X coord: " << x << std::endl;
 		std::cout << "Y coord: " << y << std::endl;
 		std::cout << "Hp: " << hp << std::endl;
-		std::cout << "Doing: " << doing << std::endl;
+		std::cout << "Doing (is alive): " << doing << std::endl;
 	}
 
-	std::string getName(std::string name) {
+	std::string getName() const {
 		return name;
 	}
-	float getX(float x) {
+	float getX() const {
 		return x;
 	}
-	float getY(float y) {
+	float getY() const {
 		return y;
 	}
-	unsigned getHp(unsigned hp) {
+	unsigned getHp() const {
 		return hp;
 	}
-	bool getDoing(bool doing) {
+	bool getDoing() const {
 		return doing;
 	}
 
@@ -72,10 +72,7 @@ public:
 	}
 
 	void damage(int dmg) {
-		if (doing == 0) {
-			return;
-		}
-		if (dmg < 0) {
+		if ((doing == 0) or (dmg <0 )) {
 			return;
 		}
 		if (dmg >= hp) {
@@ -87,17 +84,28 @@ public:
 		}
 	}
 	void heal(int heal) {
-		if (doing = 0) {
+		if ((doing == 0) or (heal < 0)) {
 			return;
 		}
-		if (heal < 0) {
-			return;
+		if ( (heal + hp) > 100) {
+			hp = 100;
 		}
-		hp += heal;
+		else{
+			hp += heal;
+		}
+		
 	}
 };
 
-float main() {
-	Player alex;
+int main() {
+	Player alex1;
+	Player alex2(alex1);
+	Player alex3("sanya", 0.3, 0.1, 50, 1);
+	alex3.damage(40);
+	alex3.printinfo();
+	alex3.heal(90);
+	alex3.damage(40);
+	alex3.printinfo();
+
 	return 0;
 }
